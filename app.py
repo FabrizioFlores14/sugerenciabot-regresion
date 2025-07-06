e52177c56d867140698ba93b84c0e0f769b76eaa
FabrizioFlores14 <119906969+FabrizioFlores14@users.noreply.github.com>
2025-07-05 20:45:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from model import entrenar_y_predecir
 
+app = FastAPI()
+
+class DatosEntrada(BaseModel):
+    registros: list[dict]  # Lista de diccionarios con los datos diarios
+
+@app.post("/evaluar")
+async def evaluar(data: DatosEntrada):
+    try:
+        sugerencias = entrenar_y_predecir(data.registros)
+        return {"sugerenciasp": sugerencias}
+    except Exception as e:
+        return {"error": str(e)}
